c5f9227584978fd4e368ad224a3163d99f6e584f
Hadi <hadi.alnehlawi@gmail.com>
2021-02-14 14:17:09 +0400

    update run_local.sh

--- src/run_local.sh
+++ src/run_local.sh
@@ -6,4 +6,5 @@ export REPOSITORY=flask;
 export DATABASE_URL="postgresql://postgres:postgres@localhost:5432/postgres";
 export FLASK_APP=app.py;
 export FLASK_ENV=development;
+flask db upgrade;
 flask run;
